7845590e563324d8fd136e306da1d3595d51c8ac
wuzeng <swuzeng@gmail.com>
2012-12-12 20:45:14 +0800

    modify static pages

--- template/index.php
+++ template/index.php
@@ -1,4 +1,5 @@
 <?php include 'inc/before_body.php'; ?>
 <?php include 'inc/nav.php'; ?>
-<?php include 'inc/front_product.php'; ?>
+<?php include 'inc/front_product_search.php'; ?>
+<?php include 'inc/front_product_list.php'; ?>
 <?php include 'inc/after_body.php'; ?>
